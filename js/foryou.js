d83960a6fda915367cd1ef67249b0ebc021246f3
Uting Huang <uting0517@gmail.com>
2018-07-11 17:42:05 +0800

    add foryou.js button

--- js/foryou.js
+++ js/foryou.js
@@ -1,35 +1,57 @@
 $(document).ready(function(){
 
-var $window = $(window);
-var $doc = $(document);
-var $quote = $(".quote");
-var $btn = $("#fy-scroll a");
+  var $window = $(window);
+  var $doc = $(document);
+  var $para = $(".para");
+  var $btn = $("#fy-scroll a");
 
-console.log($quote.offset().top);
+  $btn.click(function(){
+    
+      
+    })
+  
+  $window.on("load scroll resize",function(){
+    $para.each(function(){
+      var quoteTop = $(this).offset().top;
+      var setH = 100;
+      var $windowH = $window.height();
+    
+      if($window.scrollTop() >= ( quoteTop + setH)-$windowH){
+        $(this).animate({opacity:"1"},700);
+      };
+  
+    })
+  }
+  
+  )})
+  
+  var cursor = "<span>|</span>";
+  var count = 0;
+  var words =document.querySelector("#title-box");
+  var content = "#preparing";
+  
+  
+  function typing(){
+  if(count<=content.length){
+         words.removeAttribute("hidden");
+         words.innerHTML = content.substring(0,count) +  "<span>|</span>";
+         count++;
+         setTimeout("typing()",300);
+  }else{
+      count = 0;
+         setTimeout("typing()",5500);
+  }
+  }
+  
+     
+  typing();
 
-$btn.click(function(){
-   $("html").animate({scrollTop: 200},1000,"swing");
- })
 
-})
+  var quote = document.querySelectorAll(".quote");
+  console.log(quote);
+  console.log(quote[0].offsetTop);
+  console.log(quote[1].offsetTop);
+  console.log(quote[2].offsetTop);
+  console.log(quote[3].offsetTop);
 
-var cursor = "<span>|</span>";
-var count = 0;
-var words =document.querySelector("#title-box");
-var content = "#preparing";
-
-
-function typing(){
-if(count<=content.length){
-       words.removeAttribute("hidden");
-       words.innerHTML = content.substring(0,count) +  "<span>|</span>";
-       count++;
-       setTimeout("typing()",300);
-}else{
-    count = 0;
-       setTimeout("typing()",5500);
-}
-}
-
-   
-typing();
+  
